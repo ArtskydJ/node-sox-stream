139284fbd94030cf3b3f812f66bd036e32a75b36
Joseph Dykstra <josephdykstra@gmail.com>
2014-12-22 22:55:52 -0600

    depend on onetime

--- streamToTempFile.js
+++ streamToTempFile.js
@@ -1,9 +1,10 @@
 var fs = require('fs')
 var os = require('os')
 var path = require('path')
+var once = require('onetime')
 
-module.exports = function (cb) {
-	cb = onlyCalledOnce(cb)
+module.exports = function (callback) {
+	cb = once(callback)
 	var tempFilePath = generateTempFilePath()
 	var writeStream = fs.createWriteStream( tempFilePath )
 	writeStream.once('finish', function () {
@@ -12,23 +13,13 @@ module.exports = function (cb) {
 	writeStream.once('error', cb)
 	return writeStream
 
-	function cleanup(cb) {
+	function cleanup(cb2) {
 		fs.unlink(tempFilePath, function (err) {
-			cb && cb() //ignore deletion errors
+			cb2 && cb2() //ignore deletion errors
 		})
 	}
 }
 
 function generateTempFilePath() {
 	return path.join(os.tmpdir(), Math.random().toString().slice(2))
-}
-
-function onlyCalledOnce(cb) {
-	var calledYet = false
-	return function () {
-		if (!calledYet) {
-			calledYet = true
-			cb.apply(null, arguments)
-		}
-	}
 }

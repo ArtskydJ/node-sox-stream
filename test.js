8ada99b6d6b7eab26bd10c449b96a6047e89b68a
Joseph Dykstra <josephdykstra@gmail.com>
2015-07-11 14:20:53 -0500

    apparently stream-volume does not work for this

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var Sox = require('./')
 var fs = require('fs')
 var crypto = require('crypto')
-var volume = require('stream-volume')
+var concat = require('concat-stream')
 var path = require('path')
 var osTmpdir = require('os-tmpdir')
 var audioPath = path.dirname( require.resolve('test-audio') )
@@ -10,12 +10,12 @@ var audioPath = path.dirname( require.resolve('test-audio') )
 var tmpFilesThen = getNumOfTmpFiles()
 
 function getNumOfTmpFiles() {
-	return fs.readdirSync(osTmpdir( )).length
+	return fs.readdirSync(osTmpdir()).length
 }
 
 function closeEnough(t, got, expect, within, whats) {
-	var msg = 'got ' + got + ' ' + whats + ' is close enough to ' + expect + ' ' + whats
-	t.ok(Math.abs(got - expect) < within, msg)
+	var msg = got + ' ' + whats + ' is close enough to ' + expect + ' ' + whats
+	t.ok(Math.abs(got - expect) <= within, msg)
 	t.end()
 }
 
@@ -31,8 +31,8 @@ function asserting(soxOptsArr, file, endSize) {
 		var filePath = path.join(audioPath, file)
 		fs.createReadStream(filePath)
 			.pipe(soxTransform)
-			.pipe(volume(function close(byteNum) {
-				closeEnough(t, byteNum, endSize, endSize/200, 'bytes')
+			.pipe(concat(function close(buf) {
+				closeEnough(t, buf.length, endSize, endSize/200, 'bytes')
 			}))
 	}
 }
@@ -55,5 +55,5 @@ test('wav > ogg', asserting([{ type: 'wav' }, { type: 'ogg' }], 'test_5.wav', 18
 test('flac > ogg', asserting([{ type: 'ogg' }], 'test_7.flac', 265597 ))
 
 test('cleans up tmp files', function (t) {
-	closeEnough(t, tmpFilesThen, getNumOfTmpFiles(), 'files')
+	closeEnough(t, tmpFilesThen, getNumOfTmpFiles(), 1, 'files')
 })
